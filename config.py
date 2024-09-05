d7d79000eca172ed769f50c446d79e999e1e9755
Ramjisahu302000 <ramsahu30032000+A2@gmail.com>
2024-09-05 11:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7547160955:AAH2IbO1h8VFKmjTilozGhWCc7b1mty9Ews")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Arov_agent")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1137653039")
 
 
 
